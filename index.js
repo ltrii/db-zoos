51a3ad3e00ef9c1303a191a45731b0b7ea356977
ltrii <lelandtr@gmail.com>
2019-02-19 11:53:29 -0800

    Added Knex File and set up configuration

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 const express = require('express');
 const helmet = require('helmet');
 
+const knex = require('knex');
+const knexConfig = require('./knexfile');
+
+const db = knex(knexConfig.development);
+
+
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+
+server.post('/api/zoos', (req, res) => {
+  const zoo = req.body;
+
+  db.insert(zoo)
+    .into('zoos')
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
 
 const port = 3300;
 server.listen(port, function() {
